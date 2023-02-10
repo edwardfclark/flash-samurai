926400bc0eb052d765efc5678060ba54c126f9cb
edwardfclark <edward@streamelements.com>
2023-02-10 14:21:07 +0000

    Install prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import { json } from 'body-parser'
-import { cardRouter } from './routes/cards'
+import express from 'express';
+import { json } from 'body-parser';
+import { cardRouter } from './routes/cards';
 
-const app = express()
-app.use(json())
-app.use(cardRouter)
+const app = express();
+app.use(json());
+app.use(cardRouter);
 
 app.listen(3000, () => {
-    console.log('server is listening on port 3000')
-})
+  console.log('server is listening on port 3000');
+});
--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from 'express'
+import express, { type Request, type Response } from 'express';
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/api/card', (req: Request, res: Response) => {
-    return res.send('the card')
-})
+  return res.send('the card');
+});
 
 router.post('/api/card', (req: Request, res: Response) => {
-    return res.send('new card created')
-})
+  return res.send('new card created');
+});
 
-export { router as cardRouter }
+export { router as cardRouter };
