32d7f2377030510b614fd2f455a816880dd1e48a
edwardfclark <edward@streamelements.com>
2023-02-10 14:01:44 +0000

    Further refine card router

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { json } from 'body-parser'
+import { cardRouter } from './routes/cards'
 
 const app = express()
 app.use(json())
+app.use(cardRouter)
 
 app.listen(3000, () => {
     console.log('server is listening on port 3000')
--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,12 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
-router.get('/api/card', (req, res) => {
+router.get('/api/card', (req: Request, res: Response) => {
     return res.send('the card')
 })
 
-router.post('/api/card', (req, res) => {
+router.post('/api/card', (req: Request, res: Response) => {
     return res.send('new card created')
 })
 
