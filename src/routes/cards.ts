ba5a85e1b0fb903476723fbc17411b8546bece73
edwardfclark <edward@streamelements.com>
2023-02-10 20:37:36 +0000

    Error handling in the request

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -10,9 +10,13 @@ router.get('/api/card', (req: Request, res: Response) => {
 router.post('/api/card', async (req: Request, res: Response) => {
   const { question, answer, group } = req.body;
 
-  const card = Card.build({ question, answer, group });
-  await card.save();
-  return res.status(201).send(card);
+  try {
+    const card = Card.build({ question, answer, group });
+    await card.save();
+    return res.status(201).send(card);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 });
 
 export { router as cardRouter };
