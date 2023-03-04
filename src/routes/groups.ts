da519abc86b6ff81dec7dedbd26d8b4f8e40f207
edwardfclark <edward@streamelements.com>
2023-03-04 12:08:07 +0000

    Create group cards list endpoint, with pagiantion and tests

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from 'express';
 import { Group } from '../models/group';
+import { Card, ICard } from '../models/card';
 
 const router = express.Router();
 
@@ -50,5 +51,36 @@ router.delete('/api/group/:id', async (req: Request, res: Response) => {
     return res.status(500).send(err);
   }
 });
+
+router.get(
+  '/api/group/:id/cards',
+  async (
+    req: Request<
+      { id: string },
+      { data: ICard[]; page: number; limit: number; total: number },
+      unknown,
+      { limit: string; page: string }
+    >,
+    res: Response
+  ) => {
+    const { id } = req.params;
+    const { limit, page } = req.query;
+    const parsedLimit = parseInt(limit ?? 10, 10);
+    const parsedPage = parseInt(page ?? 1, 10);
+
+    try {
+      const cards = await Card.find({ group: id })
+        .limit(parsedLimit * 1)
+        .skip((parsedPage - 1) * parsedLimit)
+        .exec();
+
+      const total = await Card.countDocuments({ group: id });
+
+      return res.status(201).send({ data: cards, page: parsedPage, limit: parsedLimit, total });
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
+);
 
 export { router as groupRouter };
---
+++ tests/group.endpoint.cards.list.test.ts
@@ -0,0 +1,105 @@
+import { Group, IGroup } from '../src/models/group';
+import { Card, ICard } from '../src/models/card';
+
+import { connect, clearDatabase, closeDatabase } from './db';
+import { Document } from 'mongoose';
+import { app } from '../src/app';
+import request from 'supertest';
+import { Server } from 'http';
+
+const groupArgs: IGroup = {
+  name: 'Test',
+  description: 'This is a description for the Test group',
+};
+
+let group: Document;
+let application: Server;
+const wrongID = 'do_not_fetch';
+
+beforeAll(async () => {
+  application = await app.listen(0, () => {});
+
+  await connect();
+});
+
+beforeEach(async () => {
+  group = Group.build(groupArgs);
+  await group.save();
+
+  const testCards: ICard[] = [
+    {
+      question: 'question1',
+      answer: 'answer1',
+      group: group._id,
+    },
+    {
+      question: 'question2',
+      answer: 'answer2',
+      group: group._id,
+    },
+    {
+      question: 'question3',
+      answer: 'answer3',
+      group: wrongID,
+    },
+  ];
+
+  testCards.forEach(async (cardArgs) => {
+    const card = Card.build(cardArgs);
+    await card.save();
+  });
+});
+
+afterEach(async () => await clearDatabase());
+
+afterAll(async () => {
+  await closeDatabase();
+  await application.close();
+});
+
+describe('/api/group/:id/cards GET', () => {
+  it('fetches multiple cards', async () => {
+    const { _id: id } = group;
+    const res = await request(application).get(`/api/group/${id}/cards`);
+    const { body } = res;
+    const { data } = body;
+
+    expect(data.length).toBe(2);
+  });
+  it('only includes cards with the correct group', async () => {
+    const { _id: id } = group;
+    const res = await request(application).get(`/api/group/${id}/cards`);
+    const { body } = res;
+    const { data, total } = body;
+
+    expect(total).toBe(2);
+    expect(Boolean(data.find((item: ICard) => item.question === 'question3'))).toBe(false);
+  });
+  it('has a default value of 10 for limit, and a default of 1 for page', async () => {
+    const { _id: id } = group;
+    const res = await request(application).get(`/api/group/${id}/cards`);
+    const { body } = res;
+    const { limit, page } = body;
+
+    expect(limit).toBe(10);
+    expect(page).toBe(1);
+  });
+  it('allows you to request a higher limit', async () => {
+    const { _id: id } = group;
+    const res = await request(application).get(`/api/group/${id}/cards?limit=100`);
+    const { body } = res;
+    const { data, limit } = body;
+
+    expect(limit).toBe(100);
+    expect(data.length).toBe(2);
+  });
+  it('allows you to request a higher page', async () => {
+    const { _id: id } = group;
+    const res = await request(application).get(`/api/group/${id}/cards?page=2`);
+    const { body } = res;
+    const { data, page } = body;
+
+    expect(page).toBe(2);
+    expect(data.length).toBe(0);
+  });
+});
