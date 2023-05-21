e42a34fdb96e62ce5c6d2934fce9f148c3b5520d
edwardfclark <edward@streamelements.com>
2023-05-21 16:34:12 +0100

    Build out basic User schema without unit tests

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { json } from 'body-parser';
 import { cardRouter } from './routes/cards';
 import { groupRouter } from './routes/groups';
+import cors from 'cors';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,7 +11,11 @@ dotenv.config();
 const app = express();
 
 // Global middleware
+app.use(cors());
+app.use(morgan('tiny'));
 app.use(json());
+
+// Routes
 app.use(cardRouter);
 app.use(groupRouter);
 
---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+interface IUser {
+  username: string;
+  password: string;
+}
+
+interface UserDoc extends mongoose.Document {
+  username: string;
+  password: string;
+}
+
+interface userModelInterface extends mongoose.Model<UserDoc> {
+  build(user: IUser): UserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+userSchema.statics.build = (user: IUser) => {
+  return new User(user);
+};
+
+const User = mongoose.model<UserDoc, userModelInterface>('User', userSchema);
+
+export { User, IUser };
