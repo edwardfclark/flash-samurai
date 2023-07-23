3d54a1bc9e670d05c7967901cb45e5325dbb65f9
edwardfclark <edward@streamelements.com>
2023-07-23 10:29:00 +0100

    Unit tests for tag GET endpoint

--- src/utils/removeDuplicatesByKey.ts
+++ src/utils/removeDuplicatesByKey.ts
@@ -2,11 +2,13 @@
 
 /**
  * This is used to prevent items with the same key from being added to an array.
+ * Assumes the array is an array of objects.
+ * Assumes the value is a string.
  * @param arr Array of objects
  * @param key string key to prevent duplicates of
  * @returns Array of objects with duplicates removed
  */
-export function removeDuplicatesByKey(arr: Record<string, any>[], key: string) {
+export function removeDuplicatesByKey(arr: Record<string, string>[], key: string) {
   const seen: Record<string, boolean> = {};
   return arr.filter((item) => {
     return seen.hasOwnProperty(item[key]) ? false : (seen[item[key]] = true);
